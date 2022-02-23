0a3d250b9a90395b9be46f6f7e505bdfc26a021e
ethangilmore <ethanmckgilmore@gmail.com>
2022-02-22 22:51:44 -0800

    fixed HandEvaluator GetHandType bugs

--- HandsEvaluator/HandEvaluator.cs
+++ HandsEvaluator/HandEvaluator.cs
@@ -27,14 +27,14 @@
             public int CharToInt(char x)
             {
                 if (x == '0') return 10;
-                if (!double.TryParse(x.ToString(), out double a))
-                    switch (x)
-                    {
-                        case 'J': return 11;
-                        case 'Q': return 12;
-                        case 'K': return 13;
-                        case 'A': return 14;
-                    }
+                if (int.TryParse(x.ToString(), out int a)) return a;
+                switch (x)
+                {
+                    case 'J': return 11;
+                    case 'Q': return 12;
+                    case 'K': return 13;
+                    case 'A': return 14;
+                }
                 throw new ArgumentException($"Invalid card value: {x}");
             }
 
@@ -44,7 +44,7 @@
             }
         }
 
-        private static HandType GetHandType(ISet<string> cards)
+        public static HandType GetHandType(ISet<string> cards)
         {
             if (cards == null) throw new ArgumentNullException("cards cant be null");
 
@@ -52,17 +52,21 @@
             sortedCards.Sort(_comparer);
 
             List<int> matches = (List<int>)LengthOfAllMatches(sortedCards);
-            bool isFlush = IsFlush(sortedCards);
-            bool isStraight = IsStraight(sortedCards);
+            bool isFlush = IsFlush(sortedCards, out int flushHigh);
+            bool isStraight = IsStraight(sortedCards, out int straightHigh);
 
-            // TODO: implement Royal Flush
-            if (isFlush && isStraight) return HandType.StraightFlush;
+            if (isFlush && isStraight && flushHigh == straightHigh)
+                if (flushHigh == 14)
+                    return HandType.RoyalFlush;
+                else
+                    return HandType.StraightFlush;
             if (matches.Contains(4)) return HandType.FourOfKind;
             if (matches.Contains(3) && matches.Contains(2)) return HandType.FullHouse;
             if (isFlush) return HandType.Flush;
             if (isStraight) return HandType.Straight;
             if (matches.Contains(3)) return HandType.ThreeOfKind;
-            if (matches.Remove(2) && matches.Contains(2)) return HandType.TwoPair;
+            if (matches.Count(x => x == 2) == 2) return HandType.TwoPair;
+            if (matches.Contains(2)) return HandType.Pair;
             return HandType.HighCard;
         }
 
@@ -70,52 +74,65 @@
         {
             IList<int> matches = new List<int>();
 
-            int currentMatchLength = 0;
+            int currentMatchLength = 1;
 
-            for (int i = 0; i < cards.Count - 1; i++)
-                for (int j = i + i; j < cards.Count; j++)
+            int i = 0;
+            while(i < cards.Count - 1)
+                for (int j = i + 1; j < cards.Count; j++)
                     if(cards[i][0] == cards[j][0])
                         currentMatchLength++;
                     else
                     {
                         matches.Add(currentMatchLength);
-                        currentMatchLength = 0;
+                        currentMatchLength = 1;
+                        i = j;
                         break;
                     }
+                i++;
 
             return (IList<int>) matches;
         }
 
-        private static bool IsStraight(IList<string> cards)
+        private static bool IsStraight(IList<string> cards, out int high)
         {
             Func<string, int> val = x => _comparer.CharToInt(x[0]);
 
             for (int i = 0; i < 3; i++)
-                for (int j = i + 1; j < i + 6; i++)
+                for (int j = i + 1; j < i + 6; j++)
                     if (val(cards[j]) != val(cards[i]) + (j - i)) break;
-                    else if (j == i + 5) return true;
+                    else if (j == i + 5)
+                    {
+                        high = j;
+                        return true;
+                    }
+            high = -1;
             return false;
         }
 
-        private static bool IsFlush(IList<string> cards)
+        private static bool IsFlush(IList<string> cards, out int high)
         {
             int[] suits = new int[4];
             foreach (string card in cards)
                 switch (card[1])
                 {
                     case 'D':
+                        high = suits.Max();
                         if (++suits[0] == 5) return true;
                         break;
                     case 'H':
+                        high = suits.Max();
                         if (++suits[1] == 5) return true;
                         break;
                     case 'S':
+                        high = suits.Max();
                         if (++suits[2] == 5) return true;
                         break;
                     case 'C':
+                        high = suits.Max();
                         if (++suits[3] == 5) return true;
                         break;
                 }
+            high = -1;
             return false;
         }
     }
